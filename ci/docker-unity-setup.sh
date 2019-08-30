9320179f9586f92e2eedc5262acdc6d15c964c5e
SirStoke <sandro.mosca.dev@gmail.com>
2019-08-30 03:19:43 +0200

    Updated license file

--- ci/docker-unity-setup.sh
+++ ci/docker-unity-setup.sh
@@ -4,4 +4,4 @@ docker run -it --rm \
 	-e "UNITY_SERIAL=UU-DDAI-KAO-LOUIA-QQQQ-4444" \
 	-e "TEST_PLATFORM=linux" \
 	-e "WORKDIR=/root/project" \
-	-v "$(pwd)/..:/root/project" unity-android bash
+	-v "$(pwd)/..:/root/project" gableroux/unity3d:2019.2.2f1-android bash
