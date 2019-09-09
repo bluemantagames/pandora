bee0387e637e9ca4883e1056546ed75d6fc0f030
SirStoke <sandro.mosca.dev@gmail.com>
2019-09-09 21:40:00 +0200

    License

--- ci/generateLicenseRequest.sh
+++ ci/generateLicenseRequest.sh
@@ -1,1 +1,1 @@
-docker run -ti --rm -v "$(pwd):/root" -w /root /root/project/ci gableroux/unity3d:2019.2.2f1-android /opt/unity/editor/unity -batchmode -nographics -logfile - -createManualActivationFile
+docker run -ti --rm -v "$(pwd):/root" -w /root unity-android /opt/Unity/Editor/Unity -batchmode -nographics -logfile - -createManualActivationFile
