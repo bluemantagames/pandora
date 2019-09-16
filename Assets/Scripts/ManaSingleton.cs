0241c52b95358f39deab82fb80c71224943971bc
Diego Pasquali <imdiegopasquali@gmail.com>
2019-09-16 18:38:23 +0200

    Fixed ManaSingleton update issue and added `manaUnit`

--- Assets/Scripts/ManaSingleton.cs
+++ Assets/Scripts/ManaSingleton.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class ManaSingleton
 {
-    public static float manaValue { get; private set; }
+    public static float manaValue { get; private set; } = 0;
     public static float maxMana = 100f;
     public static float minMana = 0f;
 
-    public static void updateMana(float newValue)
+    // This is only used in dev
+    public static float manaUnit { get; set; } = 0;
+
+    public static void UpdateMana(float newValue)
     {
-        if (newValue < minMana)
+        if (newValue <= minMana)
         {
             manaValue = minMana;
+            return;
         }
 
-        if (newValue > maxMana)
+        if (newValue >= maxMana)
         {
             manaValue = maxMana;
+            return;
         }
 
         manaValue = newValue;
