a550453e258bcc02bb3f579c90421cec118cb54f
SirStoke <sandro.mosca.dev@gmail.com>
2019-12-01 23:21:32 +0100

    Exclam point marker script

---
+++ Assets/Scripts/AggroExclamPointBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pandora
+{
+    public class AggroExclamPointBehaviour : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
