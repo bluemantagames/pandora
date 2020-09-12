f5b454902948892db84d0e2865cc25393e5c61fe
Diego Pasquali <imdiegopasquali@gmail.com>
2020-09-12 19:02:15 +0200

    Implemented base64 sanitization

--- Assets/Scripts/network/JWT.cs
+++ Assets/Scripts/network/JWT.cs
@@ -7,11 +7,37 @@ namespace Pandora.Network
 {
     public class JWT
     {
+        /// <summary>
+        /// Sanitize an unsafe base64 string.
+        /// (This function will replace invalid characters and
+        /// add a padding using '=')
+        /// </summary>
+        /// <param name="base64Encoded">A non-sanitized base64 string</param>
+        /// <returns>A sanitized base64 string</returns>
+        private string SanitizeBase64String(string base64Encoded)
+        {
+            var sanitized = base64Encoded.Replace('_', '/').Replace('-', '+');
+
+            switch (sanitized.Length % 4)
+            {
+                case 2: sanitized += "=="; break;
+                case 3: sanitized += "="; break;
+            }
+
+            return sanitized;
+        }
+
+        /// <summary>
+        /// Get the claims from a JWT token
+        /// </summary>
+        /// <param name="jwt">The JWT string</param>
+        /// <returns>The decoded JWT payload</returns>
         private string GetClaims(string jwt)
         {
             var sections = jwt.Split('.');
             var encodedClaims = sections[1];
-            var bytesClaims = Convert.FromBase64String(encodedClaims);
+            var sanitizedEncodedClaims = SanitizeBase64String(encodedClaims);
+            var bytesClaims = Convert.FromBase64String(sanitizedEncodedClaims);
             var claims = Encoding.UTF8.GetString(bytesClaims);
 
             return claims;
