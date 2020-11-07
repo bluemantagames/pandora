7a4acb7f9a8837452b8d60ecbf2384e83922228b
SirStoke <sandro.mosca.dev@gmail.com>
2020-11-07 14:11:01 +0100

    Commands documentation

--- Assets/Scripts/ConsoleCommands.cs
+++ Assets/Scripts/ConsoleCommands.cs
@@ -5,11 +5,13 @@ using QFSW.QC;
 namespace Pandora {
     public class ConsoleCommands {
         [QFSW.QC.Command("spawn-unit-params")]
+        [CommandDescription("Spawns an allied unit named <unitName> in the cell <gridCell> - Example: spawn-unit-params \"HalfOrc\" 14,7")]
         public static void SpawnUnit(string unitName, Vector2Int gridCell) {
             SpawnUnit(unitName, new GridCell(gridCell), TeamComponent.assignedTeam);
         }
 
         [QFSW.QC.Command("spawn-enemy-unit-params")]
+        [CommandDescription("Spawns an allied unit named <unitName> in the cell <gridCell> - Example: spawn-enemy-unit-params \"HalfOrc\" 14,7. BEWARE! Y position is flipped, since you are spawning _as if_ you were your opponent")]
         public static void SpawnEnemyUnit(string unitName, Vector2Int gridCell) {
             SpawnUnit(unitName, new GridCell(gridCell), TeamComponent.opponentTeam);
         }
