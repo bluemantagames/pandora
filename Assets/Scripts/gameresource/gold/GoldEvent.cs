1511672478b4cd6c430fc6409b9320abf0b25658
SirStoke <sandro.mosca.dev@gmail.com>
2020-12-02 18:51:08 +0100

    Implemented new Favors HUD

--- Assets/Scripts/gameresource/gold/GoldEvent.cs
+++ Assets/Scripts/gameresource/gold/GoldEvent.cs
@@ -1,13 +1,13 @@
 namespace Pandora.Resource.Gold
 {
-    public class GoldEvent {}
+    public class GoldEvent {
+        /// <description>Amount of gold after earnings are added in</description>
+        public int CurrentAmount;
+    }
 
     public class GoldEarned: GoldEvent {
         /// <description>Amount earned</description>
         public int AmountEarned;
-
-        /// <description>Amount of gold after earnings are added in</description>
-        public int CurrentAmount;
 
         public GoldEarned(int currentAmount, int amountEarned) {
             CurrentAmount = currentAmount;
@@ -18,8 +18,6 @@ namespace Pandora.Resource.Gold
     public class GoldSpent: GoldEvent {
         /// <description>Amount spent</description>
         public int AmountSpent;
-        /// <description>Amount of gold after earnings are added in</description>
-        public int CurrentAmount;
 
         public GoldSpent(int currentAmount, int amountSpent) {
             CurrentAmount = currentAmount;
