8dcb380cea3dff1069aed1cfbc60b2ad61437b96
Diego Pasquali <imdiegopasquali@gmail.com>
2020-12-12 14:00:17 +0100

    Integrated UIToolkit

---
+++ Assets/Scripts/ui/MainMenuController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenuController : MonoBehaviour
+{
+    private UIDocument rootDocument;
+    private VisualElement menuViewsContainer;
+    private void OnEnable()
+    {
+    }
+}
