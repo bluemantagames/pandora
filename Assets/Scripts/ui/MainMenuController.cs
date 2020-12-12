20673cdcebf731c0d95ca553edb5b04d7a67f742
Diego Pasquali <imdiegopasquali@gmail.com>
2020-12-12 14:07:20 +0100

    Created the first main controller

--- Assets/Scripts/ui/MainMenuController.cs
+++ Assets/Scripts/ui/MainMenuController.cs
@@ -5,9 +5,14 @@ using UnityEngine.UIElements;
 
 public class MainMenuController : MonoBehaviour
 {
-    private UIDocument rootDocument;
+    private VisualElement rootElement;
     private VisualElement menuViewsContainer;
     private void OnEnable()
     {
+        rootElement = GetComponent<UIDocument>().rootVisualElement;
+        menuViewsContainer = rootElement.Q("MenuViewsContainer");
+
+        menuViewsContainer.Q("HomeContainer").style.display = DisplayStyle.None;
+        menuViewsContainer.Q("ShopContainer").style.display = DisplayStyle.Flex;
     }
 }
