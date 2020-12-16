ff44f09f4fe30192c90049a3ab039e7a186f309b
Diego Pasquali <imdiegopasquali@gmail.com>
2020-12-16 17:55:22 +0100

    Fixed names and namespaces

--- Assets/Scripts/ui/Elements/Navbar/NavbarButtonController.cs
+++ Assets/Scripts/ui/Elements/Navbar/NavbarButtonController.cs
@@ -2,56 +2,59 @@ using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class NavbarButtonController
+namespace Pandora.UI.Elements.Navbar
 {
-    private Button buttonElement;
-    private Action onPressFn;
-    public bool IsActive { get; private set; } = false;
-
-    public NavbarButtonController(Button buttonElement)
+    public class NavbarButtonController
     {
-        this.buttonElement = buttonElement;
-        onPressFn = null;
+        private Button buttonElement;
+        private Action onPressFn;
+        public bool IsActive { get; private set; } = false;
 
-        Setup();
-    }
-
-    public NavbarButtonController(Button buttonElement, Action onPressFn)
-    {
-        this.buttonElement = buttonElement;
-        this.onPressFn = onPressFn;
-
-        Setup();
-    }
-
-    public void Activate()
-    {
-        IsActive = true;
-        buttonElement.style.backgroundColor = new StyleColor(new Color(255, 255, 255, 0.3f));
-    }
-
-    public void Deactivate()
-    {
-        IsActive = false;
-        buttonElement.style.backgroundColor = new StyleColor(StyleKeyword.None);
-    }
-
-    private void Setup()
-    {
-        this.buttonElement.RegisterCallback<ClickEvent>(_ => HandleClick());
-    }
-
-    private void HandleClick()
-    {
-        if (!IsActive)
+        public NavbarButtonController(Button buttonElement)
         {
-            Activate();
-            if (onPressFn != null) onPressFn();
-        }
-    }
+            this.buttonElement = buttonElement;
+            onPressFn = null;
 
-    public void Execute()
-    {
-        HandleClick();
+            Setup();
+        }
+
+        public NavbarButtonController(Button buttonElement, Action onPressFn)
+        {
+            this.buttonElement = buttonElement;
+            this.onPressFn = onPressFn;
+
+            Setup();
+        }
+
+        public void Activate()
+        {
+            IsActive = true;
+            buttonElement.style.backgroundColor = new StyleColor(new Color(255, 255, 255, 0.3f));
+        }
+
+        public void Deactivate()
+        {
+            IsActive = false;
+            buttonElement.style.backgroundColor = new StyleColor(StyleKeyword.None);
+        }
+
+        private void Setup()
+        {
+            this.buttonElement.RegisterCallback<ClickEvent>(_ => HandleClick());
+        }
+
+        private void HandleClick()
+        {
+            if (!IsActive)
+            {
+                Activate();
+                if (onPressFn != null) onPressFn();
+            }
+        }
+
+        public void Execute()
+        {
+            HandleClick();
+        }
     }
 }
--- Assets/Scripts/ui/Elements/Views/ViewsController.cs
+++ Assets/Scripts/ui/Elements/Views/ViewsController.cs
@@ -1,68 +1,70 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine.UIElements;
-using UnityEngine.UIElements.Experimental;
 
-public enum ViewContainer
+namespace Pandora.UI.Elements.Views
 {
-    HomeContainer,
-    ShopContainer
-}
-
-public class ViewsController
-{
-
-    VisualElement viewsContainer;
-    Dictionary<string, VisualElement> singleViewsContainers = new Dictionary<string, VisualElement>();
-    ViewContainer activeView = ViewContainer.HomeContainer;
-
-    public ViewsController(VisualElement rootElement)
+    public enum ViewContainer
     {
-        viewsContainer = rootElement;
-
-        foreach (VisualElement singleViewContainer in this.viewsContainer.Children())
-        {
-            var name = singleViewContainer.name;
-
-            singleViewsContainers.Add(name, singleViewContainer);
-        }
+        HomeContainer,
+        ShopContainer
     }
 
-    private void DeactivateView(VisualElement singleViewContainer, Action Callback)
+    public class ViewsController
     {
-        Func<VisualElement, float> getter = (el) => el.style.opacity.value;
 
-        singleViewContainer.experimental.animation.Start(getter, 0f, 1000, (el, opacity) =>
+        VisualElement viewsContainer;
+        Dictionary<string, VisualElement> singleViewsContainers = new Dictionary<string, VisualElement>();
+        ViewContainer activeView = ViewContainer.HomeContainer;
+
+        public ViewsController(VisualElement rootElement)
         {
-            if (opacity == 0f)
+            viewsContainer = rootElement;
+
+            foreach (VisualElement singleViewContainer in this.viewsContainer.Children())
             {
-                singleViewContainer.style.display = DisplayStyle.None;
-                Callback();
+                var name = singleViewContainer.name;
+
+                singleViewsContainers.Add(name, singleViewContainer);
             }
-        });
+        }
+
+        private void DeactivateView(VisualElement singleViewContainer, Action Callback)
+        {
+            Func<VisualElement, float> getter = (el) => el.style.opacity.value;
+
+            singleViewContainer.experimental.animation.Start(getter, 0f, 1000, (el, opacity) =>
+            {
+                if (opacity == 0f)
+                {
+                    singleViewContainer.style.display = DisplayStyle.None;
+                    Callback();
+                }
+            });
+        }
+
+        private void ActivateView(VisualElement singleViewContainer)
+        {
+            Func<VisualElement, float> getter = (el) => el.style.opacity.value;
+
+            singleViewContainer.style.display = DisplayStyle.Flex;
+            singleViewContainer.style.opacity = 0f;
+            singleViewContainer.experimental.animation.Start(getter, 1f, 1000, (el, opacity) => { });
+        }
+
+        public void Show(ViewContainer view)
+        {
+            if (view == activeView) return;
+
+            var activeContainer = singleViewsContainers[activeView.ToString()];
+            var toShowContainer = singleViewsContainers[view.ToString()];
+
+            if (activeContainer == null || toShowContainer == null) return;
+
+            DeactivateView(activeContainer, () => ActivateView(toShowContainer));
+
+            activeView = view;
+        }
+
     }
-
-    private void ActivateView(VisualElement singleViewContainer)
-    {
-        Func<VisualElement, float> getter = (el) => el.style.opacity.value;
-
-        singleViewContainer.style.display = DisplayStyle.Flex;
-        singleViewContainer.style.opacity = 0f;
-        singleViewContainer.experimental.animation.Start(getter, 1f, 1000, (el, opacity) => { });
-    }
-
-    public void Show(ViewContainer view)
-    {
-        if (view == activeView) return;
-
-        var activeContainer = singleViewsContainers[activeView.ToString()];
-        var toShowContainer = singleViewsContainers[view.ToString()];
-
-        if (activeContainer == null || toShowContainer == null) return;
-
-        DeactivateView(activeContainer, () => ActivateView(toShowContainer));
-
-        activeView = view;
-    }
-
 }
