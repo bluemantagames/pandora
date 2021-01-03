0ed3cdc93518bde41924cb5807ece1f1578fe71d
SirStoke <sandro.mosca.dev@gmail.com>
2021-01-03 16:42:04 +0100

    Troll assets

--- Assets/Scripts/editor/AnimationImporter.cs
+++ Assets/Scripts/editor/AnimationImporter.cs
@@ -6,6 +6,7 @@ using UnityEditor.Animations;
 using UnityEngine.U2D;
 using UnityEditor.U2D;
 using System.Linq;
+using System.IO;
 
 namespace Pandora.Editor
 {
@@ -18,17 +19,43 @@ namespace Pandora.Editor
 
             var path = EditorUtility.SaveFilePanelInProject("Choose where to save the SpriteAtlas", "atlas", "spriteatlas", "");
 
-            foreach (var obj in Selection.objects)
+            var projectFolder = Path.GetFullPath(Path.Combine(Application.dataPath, "../"));
+            var texturesPath = EditorUtility.OpenFolderPanel("Pick the texture folder", "Assets/Art/Sprites/Characters/", "");
+            var projectTexturesPath = texturesPath.Replace(projectFolder, "");
+
+            var filenames =
+                (from texturePath in Directory.GetFiles(texturesPath, "*.png")
+                select Path.GetFileName(texturePath)).ToList();
+
+            Debug.Log($"Loading from {texturesPath}, removed {projectFolder}");
+
+            var percent = 0f;
+            var index = 0f;
+
+            foreach (var filename in filenames)
             {
-                Debug.Log(obj.ToString());
+                percent = (++index / filenames.Count);
 
-                if (obj is Texture2D texture)
+                progressBar($"Processing {filename}..", percent);
+
+                var assetPath = Path.Combine(projectTexturesPath, filename);
+                var objects = AssetDatabase.LoadAllAssetsAtPath(assetPath);
+
+                foreach (var obj in objects)
                 {
-                    var sprite = AssetDatabase.LoadAssetAtPath<Sprite>(AssetDatabase.GetAssetPath(texture));
+                    Debug.Log(obj.ToString());
 
-                    SpriteAtlasExtensions.Add(sa, new UnityEngine.Object[] { sprite });
+                    if (obj is Texture2D texture)
+                    {
+                        var sprite = AssetDatabase.LoadAssetAtPath<Sprite>(AssetDatabase.GetAssetPath(texture));
+
+                        SpriteAtlasExtensions.Add(sa, new UnityEngine.Object[] { sprite });
+                    }
                 }
             }
+
+            percent = 0f;
+            index = 0f;
 
             AssetDatabase.CreateAsset(sa, path);
             AssetDatabase.SaveAssets();
@@ -37,10 +64,10 @@ namespace Pandora.Editor
             // Reload the atlas from disk, since the original one is destroyed by unity when saved
             var sprites =
                 (from packable in sa.GetPackables()
-                select packable as Sprite).ToList();
+                 select packable as Sprite).ToList();
 
             var frames = new Dictionary<int, Dictionary<int, Sprite>> { };
-            var clips = new Dictionary<int, AnimationClip> {};
+            var clips = new Dictionary<int, AnimationClip> { };
 
             Debug.Log($"Clips {frames}");
 
@@ -60,6 +87,10 @@ namespace Pandora.Editor
 
             foreach (var angle in frames.Keys)
             {
+                percent += (++index / frames.Count);
+
+                progressBar($"Processing angle {angle}..", percent);
+
                 var animClip = new AnimationClip();
                 var spriteBinding = new EditorCurveBinding();
 
@@ -100,17 +131,42 @@ namespace Pandora.Editor
 
             controller.CreateBlendTreeInController("Walking", out blendTree);
 
+            var blendIndex = controller.parameters.ToList().FindIndex((parameter) => parameter.name == "Blend");
+
+            if (blendIndex > 0)
+            {
+                controller.RemoveParameter(blendIndex);
+            }
+
+            controller.AddParameter("BlendX", AnimatorControllerParameterType.Float);
+            controller.AddParameter("BlendY", AnimatorControllerParameterType.Float);
+
             blendTree.blendType = BlendTreeType.SimpleDirectional2D;
+
+            blendTree.blendParameter = "BlendX";
+            blendTree.blendParameterY = "BlendY";
 
             var directions = 12;
 
-            for (var i = 0; i < directions - 1; i++) {
-                var direction = Quaternion.AngleAxis(i * (360f / directions), Vector3.forward) * Vector2.up;
+            for (var i = 0; i < directions; i++)
+            {
+                var direction = Quaternion.AngleAxis(-i * (360f / directions), Vector3.forward) * Vector2.up;
 
-                if (clips.ContainsKey(i)) {
+                if (clips.ContainsKey(i))
+                {
                     blendTree.AddChild(clips[i], direction);
                 }
             }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            EditorUtility.ClearProgressBar();
+        }
+
+        private static void progressBar(string message, float percent)
+        {
+            EditorUtility.DisplayProgressBar("Animation importer", message, percent);
         }
     }
 
