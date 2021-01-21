afa7d54e4b1d99f99b2b8924a88ec09c8fd270e8
Diego Pasquali <imdiegopasquali@gmail.com>
2021-01-22 00:00:19 +0100

    Created menu modal

---
+++ Assets/Scripts/ui/menu/deck/SingleCardDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Pandora.UI.Modal;
+
+public class SingleCardDetails : MonoBehaviour, IPointerClickHandler
+{
+    public MenuModalBehaviour modalContainer;
+    public GameObject cardDetailsModal;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        modalContainer.AppendComponent(cardDetailsModal);
+        modalContainer.Show();
+    }
+}
---
+++ Assets/Scripts/ui/modal/MenuModalBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Pandora.UI.Modal
+{
+    public class MenuModalBehaviour : MonoBehaviour
+    {
+        public float fadeDuration = 0.2f;
+        public GameObject Viewport;
+        private Canvas canvasComponent;
+        private CanvasGroup canvasGroupComponent;
+
+        void Awake()
+        {
+            canvasComponent = GetComponent<Canvas>();
+            canvasGroupComponent = GetComponent<CanvasGroup>();
+
+            HideNoAnimation();
+        }
+
+        public void HideNoAnimation()
+        {
+            if (canvasComponent == null) return;
+
+            canvasComponent.enabled = false;
+        }
+
+        public void Hide()
+        {
+            if (canvasComponent == null || canvasGroupComponent == null) return;
+
+            canvasGroupComponent.DOFade(0, fadeDuration).OnComplete(() =>
+            {
+                canvasComponent.enabled = false;
+            });
+        }
+
+        public void Show()
+        {
+            if (canvasComponent == null || canvasGroupComponent == null) return;
+
+            canvasGroupComponent.alpha = 0;
+            canvasComponent.enabled = true;
+            var canvasFade = canvasGroupComponent.DOFade(1, fadeDuration);
+        }
+
+        public void AppendComponent(GameObject externalComponent)
+        {
+            externalComponent.transform.parent = Viewport.transform;
+            externalComponent.transform.localPosition = new Vector2(0, 0);
+        }
+    }
+}
