2799ea834ca9918dc9590608a0015580b02d71a5
Diego Pasquali <imdiegopasquali@gmail.com>
2021-02-12 19:27:39 +0100

    Implemented EULA

---
+++ Assets/Scripts/ui/menu/eula/EulaModalBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EulaModalBehaviour : MonoBehaviour
+{
+    Canvas eulaCanvas;
+    string eulaKey = "isEulaAccepted1";
+    int eulaAcceptedValue = 1;
+    int eulaNonAcceptedValue = 0;
+
+    private bool IsAlreadyAccepted()
+    {
+        var eulaValue = PlayerPrefs.GetInt(eulaKey, eulaNonAcceptedValue);
+        Logger.Debug($"Is Eula accepted: {eulaValue}");
+        return eulaValue == eulaAcceptedValue;
+    }
+
+    private void Show()
+    {
+        eulaCanvas.enabled = true;
+    }
+
+    private void Hide()
+    {
+        eulaCanvas.enabled = false;
+    }
+
+    public void AcceptEula()
+    {
+        PlayerPrefs.SetInt(eulaKey, eulaAcceptedValue);
+        Hide();
+    }
+
+    void Awake()
+    {
+        var isEulaAccepted = IsAlreadyAccepted();
+
+        eulaCanvas = GetComponent<Canvas>();
+
+        if (!isEulaAccepted) Show();
+        else Hide();
+    }
+}
