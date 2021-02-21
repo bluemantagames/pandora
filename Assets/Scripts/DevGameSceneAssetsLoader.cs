73be1aaab84201ed4ca6aa997ee48668d82052c8
Diego Pasquali <imdiegopasquali@gmail.com>
2021-02-21 16:32:27 +0100

    Fixed addressables loading in game scene and cockatrice shadow

---
+++ Assets/Scripts/DevGameSceneAssetsLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Pandora;
+using Cysharp.Threading.Tasks;
+
+public class DevGameSceneAssetsLoader : MonoBehaviour
+{
+
+    bool isExecuted = false;
+
+    private async UniTask StartLoading()
+    {
+        await AddressablesSingleton.instance.LoadUnits();
+    }
+
+    void Update()
+    {
+        if (!isExecuted)
+        {
+            isExecuted = true;
+            _ = StartLoading();
+        }
+    }
+}
