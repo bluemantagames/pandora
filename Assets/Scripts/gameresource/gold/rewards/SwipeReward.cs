e142c52583910bde5ed06fb19f9493d94146c0a6
SirStoke <sandro.mosca.dev@gmail.com>
2021-03-06 16:46:57 +0100

    Wrath VFX

--- Assets/Scripts/gameresource/gold/rewards/SwipeReward.cs
+++ Assets/Scripts/gameresource/gold/rewards/SwipeReward.cs
@@ -3,10 +3,17 @@ using Pandora;
 using Pandora.Engine;
 using Pandora.Combat;
 using System.Collections.Generic;
+using System;
 
 namespace Pandora.Resource.Gold.Rewards {
+
     public class SwipeReward : MonoBehaviour, GoldReward
     {
+        // Mapping between sector "center" position and its extreme points
+        Dictionary<Vector2Int, (Vector2Int, Vector2Int)> sectors = new Dictionary<Vector2Int, (Vector2Int, Vector2Int)>(1000);
+        public float SectorDistanceThreshold = 2000f; 
+        public GameObject SmiteVFX;
+
         public void Start() {
             RewardsRepository.Instance.Register(this);
         }
@@ -29,8 +36,53 @@ namespace Pandora.Resource.Gold.Rewards {
 
                 if (towerComponent != null) continue;
 
+                Vector2Int? sectorPosition = null;
+
+                foreach (var existingSector in sectors.Keys) {
+                    var distance = Vector2Int.Distance(entity.Position, existingSector);
+
+                    if (distance < SectorDistanceThreshold) {
+                        sectorPosition = existingSector;
+
+                        break;
+                    }
+                }
+
+                if (sectorPosition.HasValue) {
+                    var (lowerExtreme, higherExtreme) = sectors[sectorPosition.Value];
+                    
+                    lowerExtreme.Set(
+                        Math.Min(lowerExtreme.x, entity.Position.x),
+                        Math.Min(lowerExtreme.y, entity.Position.y)
+                    );
+
+                    higherExtreme.Set(
+                        Math.Max(higherExtreme.x, entity.Position.x),
+                        Math.Max(higherExtreme.y, entity.Position.y)
+                    );
+                } else {
+                    sectors[entity.Position] = (entity.Position, entity.Position);
+                }
+
                 lifeComponent.Kill(source);
             }
+
+            // for every sector, find the middle point and spawn the vfx there
+            foreach (var sectorKey in sectors.Keys) {
+                var (lo, hi) = sectors[sectorKey];
+
+                var position = new Vector2Int(
+                    (lo.x + hi.x) / 2, (lo.y + hi.y) / 2
+                );
+
+                Instantiate(
+                    SmiteVFX,
+                    map.engine.PhysicsToMapWorld(position),
+                    SmiteVFX.transform.rotation
+                );
+            }
+
+            sectors.Clear();
         }
     }
 }
