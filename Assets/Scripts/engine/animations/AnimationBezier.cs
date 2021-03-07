504427b3de8091dbdd02d88d667f215e898c9fb9
Diego Pasquali <imdiegopasquali@gmail.com>
2021-03-07 16:45:26 +0100

    Created curve serializer

---
+++ Assets/Scripts/engine/animations/AnimationBezier.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pandora.Engine.Animations
+{
+    public class AnimationBezier : MonoBehaviour
+    {
+        public AnimationCurve Curve;
+        public float SerializationTimeStep = 0.1f;
+        public float SerializationMaxTime = 1f;
+
+        public List<AnimationStep> GetSteps(int speed)
+        {
+            var steps = new List<AnimationStep>();
+
+            for (var step = SerializationTimeStep; step <= SerializationMaxTime; step += SerializationTimeStep)
+            {
+                var stepPercentage = step * 100;
+                var computedSpeed = speed * Curve.Evaluate(step);
+
+                var animationStep = new AnimationStep
+                {
+                    stepPercentage = (int)stepPercentage,
+                    speed = $"{computedSpeed}"
+                };
+
+                steps.Add(animationStep);
+            }
+
+            return steps;
+        }
+    }
+}
---
+++ Assets/Scripts/engine/animations/AnimationBezierEditor.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Pandora.Engine.Animations;
+using System.IO;
+
+[CustomEditor(typeof(AnimationBezier))]
+public class AnimationBezierEditor : Editor
+{
+    override public void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        AnimationBezier animationBezier = (AnimationBezier)target;
+
+        if (GUILayout.Button("Serialize animation"))
+        {
+            Debug.Log("Generating animation...");
+            var steps = animationBezier.GetSteps(130);
+            var collection = new AnimationStepCollection { steps = steps };
+
+            Debug.Log("Serializing animation to JSON...");
+            var serializedSteps = JsonUtility.ToJson(collection);
+
+            CreateAnimationFile(serializedSteps, "testAnimation");
+        }
+    }
+
+    private void CreateAnimationFile(string animationJson, string animationName)
+    {
+        var projectPath = Application.dataPath;
+        var animationPath = $"{projectPath}/GeneratedAnimations/{animationName}.json";
+
+        Debug.Log($"Generating animation file: {animationPath}");
+
+        try
+        {
+            using (StreamWriter sw = File.CreateText(animationPath))
+            {
+                sw.WriteLine(animationJson);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Error while writing the animation file: {e.Message}");
+        }
+    }
+}
