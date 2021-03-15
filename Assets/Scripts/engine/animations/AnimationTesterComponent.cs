315ac06f72581dbf93320bd8e6a14598396d9f94
Diego Pasquali <imdiegopasquali@gmail.com>
2021-03-15 19:47:45 +0100

    Implemented `AnimationTesterComponent`

---
+++ Assets/Scripts/engine/animations/AnimationTesterComponent.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Pandora.Movement;
+using Cysharp.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+
+namespace Pandora.Engine.Animations
+{
+    public class AnimationTesterComponent : MonoBehaviour
+    {
+        public string UnitName;
+        public int TrackStartX = 5;
+        public int TrackStartY = 4;
+        public int TrackLength = 5;
+        bool isSpawned = false;
+        bool isTurnedBack = false;
+        MapComponent mapComponent = null;
+        EngineEntity unitEntity = null;
+        string logPrefix = "[AnimationTesterComponent]";
+
+        void Start()
+        {
+            mapComponent = GetComponent<MapComponent>();
+        }
+
+        void Update()
+        {
+            if (mapComponent == null) return;
+
+            if (!isSpawned)
+            {
+                var unit = SpawnUnit();
+
+                unitEntity = unit;
+                isSpawned = true;
+            }
+
+            if (isSpawned && unitEntity != null)
+            {
+                var currentCell = unitEntity.GetCurrentCell();
+
+                if (currentCell.vector.y >= (TrackStartY + TrackLength) && !isTurnedBack)
+                {
+                    Logger.Debug($"{logPrefix} Turning back...");
+                    unitEntity.SetTarget(new GridCell(TrackStartX, TrackStartY));
+
+                    isTurnedBack = true;
+                }
+
+                if (currentCell.vector.y <= TrackStartY && isTurnedBack)
+                {
+                    Logger.Debug($"{logPrefix} Turning back...");
+                    unitEntity.SetTarget(new GridCell(TrackStartX, TrackStartY + TrackLength));
+
+                    isTurnedBack = false;
+                }
+            }
+        }
+
+        private EngineEntity SpawnUnit()
+        {
+            if (mapComponent == null) return null;
+
+            Logger.Debug($"{logPrefix} Spawning unit: {UnitName}");
+
+            mapComponent.SpawnCard(UnitName, 1, new GridCell(TrackStartX, TrackStartY));
+
+            var entities = mapComponent.engine.Entities;
+            var entity = entities[entities.Count - 1];
+
+            entity.SetEmptyPath();
+            entity.SetTarget(new GridCell(TrackStartX, TrackStartY + TrackLength));
+
+            return entity;
+        }
+    }
+}
