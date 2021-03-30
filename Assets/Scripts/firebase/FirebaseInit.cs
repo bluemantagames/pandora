4475a0dac7665005193f9b9b79a616c644114c51
SirStoke <sandro.mosca.dev@gmail.com>
2021-03-30 23:17:09 +0200

    WIP push notifications

--- Assets/Scripts/firebase/FirebaseInit.cs
+++ Assets/Scripts/firebase/FirebaseInit.cs
@@ -1,27 +1,46 @@
 using UnityEngine;
-//using Firebase;
 
 namespace Pandora.Analytics
 {
+
     public class FirebaseInit : MonoBehaviour
     {
+
+        public static Firebase.FirebaseApp app = null;
+
         void Start()
-        {/*
+        {
             Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
             {
                 var dependencyStatus = task.Result;
+
                 if (dependencyStatus == Firebase.DependencyStatus.Available)
                 {
-                    FirebaseModule.app = Firebase.FirebaseApp.DefaultInstance;
+                    Debug.Log("Firebase app loaded");
+
+                    app = Firebase.FirebaseApp.DefaultInstance;
+
+                    Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+                    Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+
+                    Firebase.Messaging.FirebaseMessaging.SubscribeAsync("/topics/matchmaking");
                 }
                 else
                 {
                     UnityEngine.Debug.LogError(System.String.Format(
                       "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
                 }
-            });*/
+            });
         }
 
+        public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+        {
+            UnityEngine.Debug.Log("Received Registration Token: " + token.Token);
+        }
 
+        public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+        {
+            UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
+        }
     }
 }
