bec450a141e994bc659e0ca8a0a3772752966c0b
SirStoke <sandro.mosca.dev@gmail.com>
2021-04-13 18:20:14 +0200

    Fixed alpha CI publish

--- misc/release-type.sh
+++ misc/release-type.sh
@@ -3,9 +3,9 @@
 MINOR="$(git describe --tags | grep -oE 'v[0-9]+\.[0-9]+' | grep -oE '.[0-9]+$' | tail -c +2)"
 
 if [[ $(($MINOR % 2)) -eq 0 ]]; then
-  RELEASE_TYPE="staging"
+  RELEASE_TYPE="internal"
 else
-  RELEASE_TYPE="production"
+  RELEASE_TYPE="alpha"
 fi
 
 echo "::set-output name=release_type::${RELEASE_TYPE}"
