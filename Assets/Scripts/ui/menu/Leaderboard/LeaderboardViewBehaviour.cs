c1364a0cc2e6f3f1b6f3064a71abe3c94ed68083
Diego Pasquali <imdiegopasquali@gmail.com>
2021-04-17 16:43:04 +0200

    Updated leaderboard structure

---
+++ Assets/Scripts/ui/menu/Leaderboard/LeaderboardViewBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using Pandora.Network;
+using Pandora;
+
+namespace Pandora.UI.Menu.Leaderboard
+{
+    public class LeaderboardViewBehaviour : MonoBehaviour
+    {
+        public int UsersPerPage = 50;
+        public GameObject SingleValueContainer;
+        public GameObject ValuesContainer;
+
+        public async UniTaskVoid LoadLeaderboard(int page)
+        {
+            var token = PlayerModelSingleton.instance.Token;
+
+            if (token == null) return;
+
+            var leaderboard = await ApiControllerSingleton.instance.GetLeaderboard(page, UsersPerPage, token);
+        }
+    }
+}
