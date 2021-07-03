8d786b2c42ce512d4d9e600d9d4a78a9addc8646
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2021-07-03 18:21:19 +0200

    Removed signup/login screens (#84)
    * Created authentication screen
    
    * Added i18n auth loading text

---
+++ Assets/Scripts/ui/auth/AuthBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Pandora;
+using Pandora.Network;
+using Cysharp.Threading.Tasks;
+using System.Net;
+using System.Collections.Generic;
+
+
+public class AuthBehaviour : MonoBehaviour
+{
+    private LoadingBehaviour loadingBehaviour;
+
+    void Awake()
+    {
+        loadingBehaviour = GameObject.Find(Constants.LOADING_CANVAS_OBJECT_NAME)?.GetComponent<LoadingBehaviour>();
+    }
+
+    void Start()
+    {
+        _ = PlayGamesAuthentication();
+    }
+
+    /// <summary>
+    /// Platform-aware play games auth caller
+    /// </summary>
+    private async UniTaskVoid PlayGamesAuthentication()
+    {
+#if UNITY_ANDROID
+        var authenticated = await PlayGames.instance.Authenticate();
+
+        if (authenticated)
+        {
+            _ = loadingBehaviour.LoadMainMenu();
+
+            AnalyticsSingleton.Instance.TrackEvent(AnalyticsSingleton.LOGIN, new Dictionary<string, object>() {
+                {"mode", "google-play"},
+                {"failed", false}
+            });
+        }
+        else
+        {
+            AnalyticsSingleton.Instance.TrackEvent(AnalyticsSingleton.LOGIN, new Dictionary<string, object>() {
+                {"mode", "google-play"},
+                {"failed", true}
+            });
+        }
+#endif
+    }
+}
